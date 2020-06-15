ffd7a8d8e6babaf6dde8a2d26709456f948f4de7
Keiichi Takahashi <bitwalk@gmail.com>
2020-06-15 21:25:39 +0900

    introduce ui_row_typical class to simplify

--- openbdfinder.py
+++ openbdfinder.py
@@ -25,92 +25,75 @@ class OpenBDFinder(Gtk.Window):
         grid.set_column_spacing(5)
         self.add(grid)
 
-        lab00 = Gtk.Label(label="ISBN13")
+        r = 0
+        str_label1 = 'ISBN13'
+        str_label2 = '書籍情報取得'
+        lab00 = Gtk.Label(label=str_label1)
         lab00.set_xalign(1.0)
         ent10 = Gtk.Entry()
         ent10.set_hexpand(True)
         ent10.set_text('9784047914742')
-        but20 = Gtk.Button(label="書籍情報取得")
+        but20 = Gtk.Button(label=str_label2)
         but20.connect("clicked", self.on_button_clicked, ent10)
 
-        lab01 = Gtk.Label(label="書影")
+        grid.attach(lab00, 0, r, 1, 1)
+        grid.attach(ent10, 1, r, 1, 1)
+        grid.attach(but20, 2, r, 1, 1)
+
+        r = 1
+        str_label = '書影'
+        lab01 = Gtk.Label(label=str_label)
         lab01.set_xalign(1.0)
         self.img11 = Gtk.Image()
         self.img11.set_halign(Gtk.Align.START)
-        lab02 = Gtk.Label(label="書籍名")
-        lab02.set_xalign(1.0)
-        self.ent12 = Gtk.Entry()
-        self.ent12.set_hexpand(True)
 
-        lab03 = Gtk.Label(label="出版社")
-        lab03.set_xalign(1.0)
-        self.ent13 = Gtk.Entry()
-        self.ent13.set_hexpand(True)
+        grid.attach(lab01, 0, r, 1, 1)
+        grid.attach(self.img11, 1, r, 2, 1)
 
-        lab04 = Gtk.Label(label="巻")
-        lab04.set_xalign(1.0)
-        self.ent14 = Gtk.Entry()
-        self.ent14.set_hexpand(True)
+        r = 2
+        str_label = '書籍名'
+        self.ent12 = ui_row_typical(grid, r, str_label)
 
-        lab05 = Gtk.Label(label="シリーズ")
-        lab05.set_xalign(1.0)
-        self.ent15 = Gtk.Entry()
-        self.ent15.set_hexpand(True)
+        r = 3
+        str_label = '出版社'
+        self.ent13 = ui_row_typical(grid, r, str_label)
 
-        lab06 = Gtk.Label(label="著者")
-        lab06.set_xalign(1.0)
-        self.ent16 = Gtk.Entry()
-        self.ent16.set_hexpand(True)
+        r = 4
+        str_label = '巻'
+        self.ent14 = ui_row_typical(grid, r, str_label)
 
-        lab07 = Gtk.Label(label="出版日")
-        lab07.set_xalign(1.0)
-        self.ent17 = Gtk.Entry()
-        self.ent17.set_hexpand(True)
+        r = 5
+        str_label = 'シリーズ'
+        self.ent15 = ui_row_typical(grid, r, str_label)
 
-        lab08 = Gtk.Label(label="サムネールURI")
-        lab08.set_xalign(1.0)
-        self.ent18 = Gtk.Entry()
-        self.ent18.set_hexpand(True)
+        r = 6
+        str_label = '著者'
+        self.ent16 = ui_row_typical(grid, r, str_label)
 
-        lab09 = Gtk.Label(label="詳細")
+        r = 7
+        str_label = '出版日'
+        self.ent17 = ui_row_typical(grid, r, str_label)
+
+        r = 8
+        str_label = 'サムネールURI'
+        self.ent18 = ui_row_typical(grid, r, str_label)
+
+        r = 9
+        str_label = '詳細'
+        lab09 = Gtk.Label(label=str_label)
         lab09.set_xalign(1.0)
+        lab09.set_valign(Gtk.Align.START)
         scr19 = Gtk.ScrolledWindow()
         self.dscrpt = Gtk.TextView()
+        self.dscrpt.set_editable(False)
+        self.dscrpt.set_can_focus(False)
         self.dscrpt.set_wrap_mode(wrap_mode=Gtk.WrapMode.WORD)
         scr19.add(self.dscrpt)
         scr19.set_hexpand(True)
         scr19.set_vexpand(True)
 
-        grid.attach(lab00, 0, 0, 1, 1)
-        grid.attach(ent10, 1, 0, 1, 1)
-        grid.attach(but20, 2, 0, 1, 1)
-
-        grid.attach(lab01, 0, 1, 1, 1)
-        grid.attach(self.img11, 1, 1, 2, 1)
-
-        grid.attach(lab02, 0, 2, 1, 1)
-        grid.attach(self.ent12, 1, 2, 2, 1)
-
-        grid.attach(lab03, 0, 3, 1, 1)
-        grid.attach(self.ent13, 1, 3, 2, 1)
-
-        grid.attach(lab04, 0, 4, 1, 1)
-        grid.attach(self.ent14, 1, 4, 2, 1)
-
-        grid.attach(lab05, 0, 5, 1, 1)
-        grid.attach(self.ent15, 1, 5, 2, 1)
-
-        grid.attach(lab06, 0, 6, 1, 1)
-        grid.attach(self.ent16, 1, 6, 2, 1)
-
-        grid.attach(lab07, 0, 7, 1, 1)
-        grid.attach(self.ent17, 1, 7, 2, 1)
-
-        grid.attach(lab08, 0, 8, 1, 1)
-        grid.attach(self.ent18, 1, 8, 2, 1)
-
-        grid.attach(lab09, 0, 9, 1, 1)
-        grid.attach(scr19, 1, 9, 2, 1)
+        grid.attach(lab09, 0, r, 1, 1)
+        grid.attach(scr19, 1, r, 2, 1)
 
     # -------------------------------------------------------------------------
     #  on_button_clicked
@@ -150,6 +133,21 @@ class OpenBDFinder(Gtk.Window):
         img = loader.get_pixbuf()
         self.img11.set_from_pixbuf(img)
 
+
+class ui_row_typical(Gtk.Entry):
+    def __init__(self, grid, row, label):
+        Gtk.Entry.__init__(self)
+        lab = Gtk.Label(label=label)
+        lab.set_xalign(1.0)
+        self.set_editable(False)
+        self.set_can_focus(False)
+        self.set_hexpand(True)
+
+        grid.attach(lab, 0, row, 1, 1)
+        grid.attach(self, 1, row, 2, 1)
+
+    #def get(self):
+    #    return self.ent
 
 win = OpenBDFinder()
 win.connect("destroy", Gtk.main_quit)
